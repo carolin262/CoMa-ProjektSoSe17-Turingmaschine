6e27184b1c826244bf09c404f38127ae4cdaaa00
Carolin Schwarz <schwarzcarolin262@gmail.com>
2017-06-20 10:32:28 +0200

    CoMA Klausurzusammenfassung von Stefan

---
+++ Lernstoff Klausur/Binärbäume/BinTree.java
@@ -0,0 +1,15 @@
+
+/**
+ * Beschreiben Sie hier die Klasse BinTree.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class BinTree
+{
+   
+    public class BinTreeNode
+    {
+        private int wert;
+        private in
+}
